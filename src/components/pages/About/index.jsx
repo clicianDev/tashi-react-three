3c4abec1af3958e3614d6a2ed9a2a3709cb2f918
ClicianDev <ceedy12345@gmail.com>
2023-10-17 16:24:20 +0800

    update about section

--- src/components/pages/About/index.jsx
+++ src/components/pages/About/index.jsx
@@ -7,7 +7,7 @@ function About() {
       id="About"
       className={`w-screen h-screen ${styles.sectionContainer} `}
     >
-      <div className={` text-center py-12 justify-center font-inter `}>
+      <div className={` text-center py-12 font-inter `}>
         <div className="hidden lg:block mb-7">
           <a className={`${styles.buttonBgTwo}`}>Integrations</a>
         </div>
@@ -16,25 +16,27 @@ function About() {
           <br /> we deliver.
         </h1>
       </div>
-      <div className="grid grid-cols-2 gap-4 about-background justify-center items-center ">
-        <div className="relative m-auto  w-screen mt-[30px] bg-red-500">
-          <img src="./images/about/Logo.svg " className="m-auto" />
-        </div>
-        {content.map((item, index) => (
-          <div
-            key={index}
-            className="py-5 justify-center font-inter text-center px-28 "
-          >
-            <img
-              src={`./images/about/icons/${item.image}.svg`}
-              className="m-auto"
-            />
-            <div className="text-white">
-              <h1 className="font-bold ">{item.title}</h1>
-              <p>{item.Description}</p>
+      <div>
+        <div className="about-icon">
+          <div className="grid grid-cols-2 gap-4  about-background justify-center ">
+          {content.map((item, index) => (
+            <div
+              key={index}
+              className="py-5 justify-center font-inter text-center px-28 "
+            >
+              <img
+                src={`./images/about/icons/${item.image}.svg`}
+                className="m-auto"
+              />
+              <div className="text-white">
+                <h1 className="font-bold ">{item.title}</h1>
+                <p>{item.Description}</p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
+        </div>
+        
       </div>
     </section>
   );
