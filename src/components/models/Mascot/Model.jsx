b222693ff7579af48828738f9257862b1a619483
clicianDev <ceedy12345@gmail.com>
2023-10-18 18:02:35 +0800

    animation clicked count update

--- src/components/models/Mascot/Model.jsx
+++ src/components/models/Mascot/Model.jsx
@@ -4,22 +4,40 @@ import { useFrame } from "@react-three/fiber";
 
 function Model(props) {
   const groups = useRef();
-  const mascot = useGLTF("./mascot-draco/mascot.gltf");
+  const mascot = useGLTF("./mascot-v2/mascot.gltf");
   const { actions, names } = useAnimations(mascot.animations, groups);
-
-  const [animationIndex, setAnimationIndex] = useState(2);
+  console.log(actions);
+  const [clickCount, setClickCount] = useState(0);
+  const [animationIndex, setAnimationIndex] = useState(5);
+  const [animationIndex2, setAnimationIndex2] = useState(10);
   const [shouldPlayAnimation1, setShouldPlayAnimation1] = useState(false);
 
   useEffect(() => {
     // Play the animation based on animationIndex
     const animation = actions[names[animationIndex]];
+    const animation2 = actions[names[animationIndex2]];
+    animation2.reset().fadeIn(0.5).play();
     animation.reset().fadeIn(0.5).play();
 
     // If shouldPlayAnimation1 is true, play Index(1) and then switch back to Index(2)
     if (shouldPlayAnimation1) {
       setTimeout(() => {
-        setShouldPlayAnimation1(false);
-        setAnimationIndex(2);
+        console.log(clickCount);
+        if (clickCount % 2 === 0) {
+          // clickCount is even
+          setShouldPlayAnimation1(false);
+          setAnimationIndex(3);
+          setAnimationIndex2(9);
+        } else {
+          if (clickCount >= 3) {
+            setAnimationIndex(4);
+            setAnimationIndex2(10);
+          } else {
+            // clickCount is odd
+            setShouldPlayAnimation1(false);
+            setAnimationIndex(5);
+          }
+        }
       }, animation._clip.duration * 900); // Set the timeout to match the duration of Index(1) animation in milliseconds
     }
 
@@ -28,20 +46,18 @@ function Model(props) {
   }, [animationIndex, actions, names, shouldPlayAnimation1]);
 
   const handleClick = (e) => {
-    setAnimationIndex(1);
+    setAnimationIndex(0);
+    setAnimationIndex2(8);
+    setClickCount(clickCount + 1);
     setShouldPlayAnimation1(true);
   };
 
   return (
-<>
-  <mesh ref={groups} castShadow receiveShadow onClick={handleClick}>
-    <primitive
-      object={mascot.scene}
-      position={[0, 0, 0]}
-     
-    />
-  </mesh>
-</>
+    <>
+      <mesh ref={groups} castShadow receiveShadow onClick={handleClick}>
+        <primitive object={mascot.scene} position={[0, 0, 0]} />
+      </mesh>
+    </>
   );
 }
 
