cbae450a16a497805966ccc87a6dba6eb396add0
clicianDev <ceedy12345@gmail.com>
2023-10-25 17:57:44 +0800

    fix path error on production

--- src/components/models/Mascot/Model.jsx
+++ src/components/models/Mascot/Model.jsx
@@ -4,7 +4,7 @@ import { useFrame } from "@react-three/fiber";
 
 function Model(props) {
   const groups = useRef();
-  const mascot = useGLTF('./public/mascot-v3-meshopt/mascot.gltf');
+  const mascot = useGLTF('/mascot-v3-meshopt/mascot.gltf');
   const { actions, names } = useAnimations(mascot.animations, groups);
   console.log(actions);
   const [clickCount, setClickCount] = useState(0);
