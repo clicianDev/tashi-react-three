2c52bdf8f749cc917f3a466dc6f814f3c1fa89ef
clicianDev <ceedy12345@gmail.com>
2023-11-13 13:24:24 +0800

    update path of about us

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="about-us" element={<About />} />
+          <Route path="/about-us" element={<About />} />
         </Route>
       </Routes>
     </BrowserRouter>
