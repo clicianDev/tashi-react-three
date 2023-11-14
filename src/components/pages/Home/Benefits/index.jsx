de213b11922a4b781e923ca14aeea883fde33f63
clicianDev <ceedy12345@gmail.com>
2023-11-14 15:07:43 +0800

    tashi homepage update

--- src/components/pages/Home/Benefits/index.jsx
+++ src/components/pages/Home/Benefits/index.jsx
@@ -9,7 +9,7 @@ function Benefits() {
       className={`max-w-screen-2xl m-auto ${styles.sectionContainer} violet-background `}
     >
       <div className="font-inter text-center p- lg:p-10">
-        <h2 className="text-5xl md:text-5xl lg:text-6xl leading-[1.5] md:leading-[1.6] font-semibold gradient-text">
+        <h2 className="text-4xl md:text-5xl lg:text-6xl leading-[1.5] md:leading-[1.6] font-semibold gradient-text">
           Benefits that we deliver.
         </h2>
         <p className="mt-4 text-base md:text-lg lg:text-xl text-white">
--- src/components/pages/Home/Solutions/index.jsx
+++ src/components/pages/Home/Solutions/index.jsx
@@ -9,20 +9,20 @@ function Solutions() {
       className={`max-w-screen-2xl m-auto relative ${styles.sectionContainer} overflow-clip`}
     >
       <div className=" font-inter">
-        <h1 className="justify-center gradient-text-2 text-center text-[54px] leading-[60px] font-semibold p-5">
+      <h2 className=" text-center text-4xl md:text-5xl lg:text-6xl leading-[1.5] md:leading-[1.6] font-semibold gradient-text">
           Tashi’s Solutions Cater to Both <br />
           Web2 & Web3 Games.
-        </h1>
+        </h2>
       </div>
       <div className="relative m-auto">
-        <div className="absolute mt-[-10%] m-auto z-0">
+        <div className="absolute mt-[8%] lg:mt-[-10%] m-auto z-0">
           <img src="\images\background-hero.png"></img>
         </div>
-        <div className="grid grid-cols-2 m-auto">
-          <div className="justify-center">
-            <img className="m-auto" src="/images/solutions/Web2.svg" />
+        <div className="grid grid-cols-2 m-auto gap-5">
+          <div className="justify-center p-5 md:p-2 lg:p-0">
+            <img className="m-auto" src="/images/solutions/Web2.svg"/>
           </div>
-          <div className="justify-center">
+          <div className="justify-center p-5 md:p-2 lg:p-0">
             <img className="m-auto" src="/images/solutions/Web3.svg" />
           </div>
         </div>
