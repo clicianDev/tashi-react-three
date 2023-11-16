d4a8e731e03fd3824663df79906851c3fe101e8e
clicianDev <ceedy12345@gmail.com>
2023-11-16 15:14:58 +0800

    tashi dGamestack

--- src/components/pages/Home/GameStack/index.jsx
+++ src/components/pages/Home/GameStack/index.jsx
@@ -5,7 +5,7 @@ function GameStack() {
   return (
     <section
       id="dGame-stack"
-      className={`max-w-screen-2xl m-auto px-4 py-20 sm:py-20 lg:py-20 sm:px-6 lg:px-20 mx-auto`}
+      className={`max-w-screen-2xl m-auto px-4 py-20 sm:py-20 lg:py-20 sm:px-6 lg:px-20 mx-auto violet-background2`}
     >
       <div className="text-center">
         <h2 className="gradient-text-2 text-4xl md:text-5xl lg:text-6xl leading-[1.5] md:leading-[1.6] font-semibold">
@@ -16,8 +16,8 @@ function GameStack() {
           industry
         </p>
       </div>
-     
-        {/* <div className="grid grid-cols-3 gap-20 h-auto m-auto  px-20">
+
+      {/* <div className="grid grid-cols-3 gap-20 h-auto m-auto  px-20">
           <div className="grid grid-cols-1 lg:grid-cols-1 gap-5 lg:gap-10 justify-center ">
             <div className="flex justify-center">
               <div className="m-auto">
@@ -100,64 +100,106 @@ function GameStack() {
             </div>
           </div>
         </div> */}
-    
-      <div className="flex flex-col lg:flex-row my-10 justify-center">
-        <div className="grid grid-cols-1 lg:grid-cols-1 gap-5 lg:gap-10 justify-center">
-          <div className="gap-2">
-            <h3 className="text-white font-normal text-xl lg:text-3xl">Play</h3>
-            <p className="text-white opacity-50 font-normal text-base lg:text-xl">
-              Cross-platform Lobby
-              <br /> & Match-making
-            </p>
+
+      <div className="flex flex-col lg:flex-row my-10 justify-center z-50">
+        <div className="grid grid-cols-1 lg:grid-cols-1 gap-5 lg:gap-10 justify-center md:m-auto">
+          <div className="flex md:block gap-2 items-center">
+            <div className="block md:hidden">
+              <img src="./images/About/icons/person.svg" />
+            </div>
+            <div>
+              <h3 className="text-white font-normal text-xl lg:text-3xl">
+                Play
+              </h3>
+              <p className="text-white opacity-50 font-normal text-base lg:text-xl">
+                Cross-platform Lobby
+                <br /> & Match-making
+              </p>
+            </div>
           </div>
-          <div className="gap-2">
-            <h3 className="text-white font-normal text-xl lg:text-3xl">Access</h3>
-            <p className="text-white opacity-50 font-normal text-base lg:text-xl">
-              3rd Party Account
-              <br />
-              Integration
-              <br />
-              GDPR & COPPA Compliance
-            </p>
+          <div className="flex md:block gap-2 items-center">
+            <div className="block md:hidden">
+              <img src="./images/About/icons/person.svg" />
+            </div>
+            <div>
+              <h3 className="text-white font-normal text-xl lg:text-3xl">
+                Access
+              </h3>
+              <p className="text-white opacity-50 font-normal text-base lg:text-xl">
+                3rd Party Account
+                <br />
+                Integration
+                <br />
+                GDPR & COPPA Compliance
+              </p>
+            </div>
           </div>
-          <div className="gap-2">
-            <h3 className="text-white font-normal text-xl lg:text-3xl">Engagement</h3>
-            <p className="text-white opacity-50 font-normal text-base lg:text-xl">
-              Leaderboards, Achievements & Rewards
-            </p>
+          <div className="flex md:block gap-2 items-center">
+            <div className="block md:hidden">
+              <img src="./images/About/icons/person.svg" />
+            </div>
+            <div>
+              <h3 className="text-white font-normal text-xl lg:text-3xl">
+                Engagement
+              </h3>
+              <p className="text-white opacity-50 font-normal text-base lg:text-xl">
+                Leaderboards, Achievements & Rewards
+              </p>
+            </div>
           </div>
         </div>
-        <div className="relative w-full lg:w-1/2 justify-center my-10">
+        <div className="relative w-full lg:w-1/2 justify-center my-10 z-50 hidden md:block lg:block ">
           <div className="justify-center w-full">
-          <img
-            src="images/gamestack/circle.svg"
-            className="m-auto"
-            alt="Circle"
-          />
+            <img
+              src="images/gamestack/circle.svg"
+              className="m-auto"
+              alt="Circle"
+            />
           </div>
         </div>
-        <div className="grid grid-cols-1 lg:grid-cols-1 gap-5 lg:gap-10 justify-center">
-          <div className="gap-2">
-            <h3 className="text-white font-normal text-xl lg:text-3xl">Storage</h3>
-            <p className="text-white opacity-50 font-normal text-base lg:text-xl">
-              Decentralized, Persistent, <br />
-              Player Profiles, Statistics Tracking
-            </p>
+        <div className="grid grid-cols-1 lg:grid-cols-1 gap-5 lg:gap-10 justify-center md:m-auto">
+          <div className="flex md:block gap-2 items-center">
+            <div className="block md:hidden">
+              <img src="./images/About/icons/person.svg" />
+            </div>
+            <div>
+              <h3 className="text-white font-normal text-xl lg:text-3xl">
+                Storage
+              </h3>
+              <p className="text-white opacity-50 font-normal text-base lg:text-xl">
+                Decentralized, Persistent, <br />
+                Player Profiles, Statistics Tracking
+              </p>
+            </div>
           </div>
-          <div className="gap-2">
-            <h3 className="text-white font-normal text-xl lg:text-3xl">Monetization</h3>
-            <p className="text-white opacity-50 font-normal text-base lg:text-xl">
-              Catalogue, Order, and Entitlements
-              <br /> Wallets & Currency Rails <br /> In-Game/App purchases
-            </p>
+          <div className="flex md:block gap-2 items-center">
+            <div className="block md:hidden">
+              <img src="./images/About/icons/person.svg" />
+            </div>
+            <div>
+              <h3 className="text-white font-normal text-xl lg:text-3xl">
+                Monetization
+              </h3>
+              <p className="text-white opacity-50 font-normal text-base lg:text-xl">
+                Catalogue, Order, and Entitlements
+                <br /> Wallets & Currency Rails <br /> In-Game/App purchases
+              </p>
+            </div>
           </div>
-          <div className="gap-2">
-            <h3 className="text-white font-normal text-xl lg:text-3xl">Dashboard</h3>
-            <p className="text-white opacity-50 font-normal text-base lg:text-xl">
-              KPI Tracking <br />
-              Custom Metric Creation, <br />
-              Data-Warehouse, Streaming
-            </p>
+          <div className="flex md:block gap-2 items-center">
+            <div className="block md:hidden">
+              <img src="./images/About/icons/person.svg" />
+            </div>
+            <div>
+              <h3 className="text-white font-normal text-xl lg:text-3xl">
+                Dashboard
+              </h3>
+              <p className="text-white opacity-50 font-normal text-base lg:text-xl">
+                KPI Tracking <br />
+                Custom Metric Creation, <br />
+                Data-Warehouse, Streaming
+              </p>
+            </div>
           </div>
         </div>
       </div>
--- src/components/pages/Home/Hero/index.jsx
+++ src/components/pages/Home/Hero/index.jsx
@@ -7,7 +7,7 @@ function Hero() {
   return (
     <section
       id="hero"
-      className={`max-w-screen-2xl m-auto relative h-full flex items-center hero-background bg-center lg:bg-right xl:bg-right 2xl:bg-right px-0 py-20 sm:py-20 lg:py-24 sm:px-6 lg:px-20 mx-auto`}
+      className={`max-w-screen-2xl m-auto relative h-full flex items-center hero-background bg-bottom bg-contain lg:bg-cover lg:bg-right xl:bg-right 2xl:bg-right px-0 py-20 sm:py-20 lg:py-24 sm:px-6 lg:px-20 mx-auto`}
     >
      
         <div className={`z-10 text-center md:text-left lg:text-left py-32 px-5 sm:px-3 lg:px-0 xl:px-0 `}>
