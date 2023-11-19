5d869dce8140463e249aae01e3218d568eb82f31
clicianDev <ceedy12345@gmail.com>
2023-11-19 17:09:11 +0800

    tashi timeline update

--- src/components/pages/Home/Timeline/index.jsx
+++ src/components/pages/Home/Timeline/index.jsx
@@ -12,8 +12,8 @@ function Timeline() {
             <div className="card-body bg-tashi-primary font-inter text-white violet-background">
               <h2 className="font-semibold text-5xl">2022</h2>
               <ul className="text-2xl leading-6 font-normal list-disc p-5">
-                <li>Cracked the consensus code</li>
-                <li>Showcased first decentralised demo game</li>
+                <li className="my-5">Cracked the consensus code</li>
+                <li className="my-5">Showcased first decentralised demo game</li>
               </ul>
             </div>
           </li>
@@ -21,19 +21,19 @@ function Timeline() {
             <div className="card-body bg-tashi-primary font-inter text-white violet-background">
               <h2 className="font-semibold text-5xl">2023</h2>
               <ul className="text-2xl leading-6 font-normal list-disc p-5">
-                <li>Secured seed funding</li>
-                <li>Launched PC and mobile gaming backend platform</li>
-                <li>Solved Netcode, NAT hole punching, Smart Relays</li>
+                <li className="my-5">Secured seed funding</li>
+                <li className="my-5">Launched PC and mobile gaming backend platform</li>
+                <li className="my-5">Solved Netcode, NAT hole punching, Smart Relays</li>
               </ul>
             </div>
           </li>
           <li className="card" id="card3">
             <div className="card-body bg-tashi-primary font-inter text-white violet-background">
               <h2 className="font-semibold text-5xl">2024</h2>
-              <ul className="text-2xl leading-6 font-normal list-disc p-5 gap-5">
-                <li>Token Generation Event</li>
-                <li>Launch global nodes and Tashi Council</li>
-                <li>Onboard 1 Million Gamers on Tashi’s infrastructure</li>
+              <ul className="text-2xl leading-6 font-normal list-disc p-5 ">
+                <li className="my-5">Token Generation Event</li>
+                <li className="my-5">Launch global nodes and Tashi Council</li>
+                <li className="my-5">Onboard 1 Million Gamers on Tashi’s infrastructure</li>
               </ul>
             </div>
           </li>
