4eac008f0d038f207f9602c880f8578adab837fd
clicianDev <ceedy12345@gmail.com>
2023-11-23 15:36:54 +0800

    Add  list of advisors under the team section

---
+++ src/components/pages/Team/components/Team/Advisors.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Team from './team.json'
+function Advisors() {
+    return ( 
+        <div className="grid sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 gap-10 w-full my-20 sm:px-5 md:px-20 lg:px-20 xl:px-32 m-auto">
+      {Team.Advisors.map((item, index) => (
+        <div
+          key={index}
+          className="border border-tashi-tertiary rounded-2xl p-2 flex gap-5 h-max w-auto "
+        >
+          <div className="border border-tashi-tertiary rounded-2xl flex gap-2 justify-center overflow-hidden w-max ">
+            <img src={`./images/About/team/advisors/${item.imgPath}.png`} width={140} height={130}/>
+          </div>
+          <div className="flex flex-col w-1/2 ">
+            <h3 className="text-white font-normal text-lg">{item.name}</h3>
+            <p className="text-gray-500 font-normal text-base">
+              {item.desc}
+            </p>
+          </div>
+        </div>
+      ))}
+    </div>
+     );
+}
+
+export default Advisors;
--- src/components/pages/Team/index.jsx
+++ src/components/pages/Team/index.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import styles from '../../styles';
-
-import TeamList from './components/Team';
+import React from "react";
+import styles from "../../styles";
+import Advisors from "./components/Team/Advisors";
+import TeamList from "./components/Team";
 
 function Team() {
   return (
@@ -18,7 +18,20 @@ function Team() {
             Seasoned professionals with specialized Web3 experience
           </p>
         </div>
-       <TeamList/>
+        <TeamList />
+        <span className="fade-border"></span>
+        <div className="violet-background bg-contain bg-top">
+          <div className="relative font-inter py-10 md:py-20 ">
+            <h2 className="justify-center gradient-text-2 text-center text-3xl md:text-4xl lg:text-5xl leading-[1.5] md:leading-[1.6] font-semibold p-5">
+              Our Advisors
+            </h2>
+            <p className="font-normal text-base md:text-lg lg:text-xl text-center text-white mx-5 lg:px-10">
+              Tashi knows what’s up, we’re advised by heavyweights of the gaming
+              sector.
+            </p>
+          </div>
+          <Advisors />
+        </div>
       </section>
     </>
   );
