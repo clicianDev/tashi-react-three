7f54438f8c2328d8694f0e37e220596fd91c25ca
clicianDev <ceedy12345@gmail.com>
2023-11-24 11:07:39 +0800

    Add linkedIn profile url  to tashi team advisors

--- src/components/pages/Team/index.jsx
+++ src/components/pages/Team/index.jsx
@@ -8,7 +8,7 @@ function Team() {
     <>
       <section
         id="Team"
-        className={`relative max-w-screen-2xl m-auto w-full h-full ${styles.sectionContainer} violet-background lg:bg-contain bg-top`}
+        className={`relative max-w-screen-2xl m-auto w-full h-full ${styles.sectionContainer} violet-background lg:bg-contain bg-top overflow-hidden`}
       >
         <div className="relative font-inter py-10 md:py-20">
           <h1 className="justify-center gradient-text-2 text-center text-3xl md:text-4xl lg:text-5xl leading-[1.5] md:leading-[1.6] font-semibold p-5">
