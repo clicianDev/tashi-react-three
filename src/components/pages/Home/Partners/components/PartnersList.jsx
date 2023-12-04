1e83fb041f62d8393306f73dc7c73ea86392095f
ClicianDev <ceedy12345@gmail.com>
2023-12-04 15:12:44 +0800

    Update on tashi team page

--- src/components/pages/Home/Partners/components/PartnersList.jsx
+++ src/components/pages/Home/Partners/components/PartnersList.jsx
@@ -6,9 +6,11 @@ function PartnersList() {
     return ( 
         <div className='grid grid-cols-3 lg:grid-flow-col my-5 text-center px-5 lg:px-36 gap-10'>
             {Data.map((item, index) => (
+              
                 <a href={item.url} key={index} target='_blank' className='m-auto'> 
                     <img width="100%" src= {`./images/partners/logo/${item.path}.svg`} />
                 </a>
+            
             ))}
         </div>
         );
